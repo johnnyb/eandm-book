e4aa62e42c87a1db51d0ba29ea4ae070b4691a87
Jonathan Bartlett <jonathan@newmedio.com>
2014-03-07 17:39:51 -0600

    Modifications

--- genkindle.rb
+++ genkindle.rb
@@ -17,21 +17,23 @@ chapters = [
 'Mignea3',
 'Sich'
 ]
-# chapters = ["Bartlett1"]
+ chapters = ["Ewert"]
 
 chapters.each do |chap|
 	puts "\n\n\n\n\nChapter #{chap}\n\n\n\n\n\n\n"
 	cwrap = "#{chap}Wrapper"
 	cwrapt = "#{cwrap}.tex"
+	system("rm -R #{cwrap}*")
 	system("cp Wrapper.tmpl #{cwrapt}")
 	system("perl -pi -e s!FILE!#{chap}!gsi #{cwrapt}")
 	puts "PDFing"
 	system("pdflatex #{cwrap}")
+	system("pdflatex #{cwrap}")
 	puts "Bibtexing"
 	system("bibtex #{cwrap}")
-	#`pdflatex #{cwrap}`
-	#`pdflatex #{cwrap}`
-	#`pdflatex #{cwrap}`
+	system("pdflatex #{cwrap}")
+	system("pdflatex #{cwrap}")
+	system("pdflatex #{cwrap}")
 	puts "htlatexing"
 	system("htlatex #{cwrapt} xhtml")
 end
