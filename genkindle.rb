5b3f1c75f7def7fdde8d8faf6f4dd724bd75cb45
Jonathan Bartlett <jonathan@newmedio.com>
2014-03-07 00:02:45 -0600

    Working on epub/kindle versions

---
+++ genkindle.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# AboutAuthors.tex
+# Acknowledgements.tex
+# TitlePages.tex
+
+chapters = [
+'Bartlett1',
+'Bartlett2',
+'Ewert',
+'Hall',
+'Halsmer',
+'Holloway',
+'Introduction',
+'Mignea1',
+'Mignea2',
+'Mignea3',
+'Sich'
+]
+
+chapters.each do |chap|
+	cwrap = "#{chap}Wrapper"
+	cwrapt = "#{cwrap}.tex"
+	`cp Wrapper.tmpl #{cwrapt}`
+	`perl -pi -e s!FILE!#{chap}!gsi #{cwrapt}`
+	`pdflatex #{cwrap}`
+	`bibtex #{cwrap}`
+	`pdflatex #{cwrap}`
+	`pdflatex #{cwrap}`
+	`pdflatex #{cwrap}`
+end
